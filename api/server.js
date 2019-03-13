3cfb6ce2a7c378403bcd99206028ff6e18f8ecd0
bhumip214 <bhumip214@gmail.com>
2019-03-13 10:41:37 -0700

    basic server testing done

--- api/server.js
+++ api/server.js
@@ -12,8 +12,8 @@ server.use(helmet());
 server.use(morgan("dev"));
 server.use("/api/invite", inviteRoute);
 
-server.get("/", async (req, res, next) => {
-  res.send(`Flextogether API`);
+server.get("/", async (req, res) => {
+  res.status(200).json("Lambda Build Week: Flextogether API");
 });
 
 module.exports = server;
