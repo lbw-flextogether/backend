eef56835793a0a9641e8915b7275a509927b0f3c
bhumip214 <bhumip214@gmail.com>
2019-03-13 17:32:39 -0700

    add api tests

--- services/tokenService.js
+++ services/tokenService.js
@@ -4,11 +4,12 @@ function generateToken(data) {
   const options = {
     expiresIn: "1d"
   };
-  return jwt.sign(data, process.env.JWT_SECRET, options);
+
+  return jwt.sign(data, process.env.JWT_SECRET || "jwt_dev_token", options);
 }
 
 function verifyToken(token) {
-  return jwt.verify(token, process.env.JWT_SECRET);
+  return jwt.verify(token, process.env.JWT_SECRET || "jwt_dev_token");
 }
 
 module.exports = {
